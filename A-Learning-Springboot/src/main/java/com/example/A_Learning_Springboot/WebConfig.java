e2e77104f7540ea083d758250a24decc3bd56750
AkramZennad <akram.zennad@univ-constantine2.dz>
2025-03-10 20:54:51 +0100

    docker6

--- A-Learning-Springboot/src/main/java/com/example/A_Learning_Springboot/WebConfig.java
+++ A-Learning-Springboot/src/main/java/com/example/A_Learning_Springboot/WebConfig.java
@@ -1,6 +1,5 @@
 package com.example.A_Learning_Springboot;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -10,12 +9,15 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") 
-                .allowedOrigins( "https://a-learning-10aaf.web.app",
-                        "*")
+        registry.addMapping("/**")
+                // Explicitly allow Flutter Web URL
+                .allowedOrigins("https://a-learning-10aaf.web.app")
+
+                // Allow mobile apps by using patterns
+                .allowedOriginPatterns("*")
+
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
 }
-
